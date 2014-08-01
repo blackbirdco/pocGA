634841beb27b37d401fa05755dfa15683ee84ccc
Martin Keogh <martin1keogh@gmail.com>
2014-08-01 10:55:45 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PocGA::Application.config.secret_key_base = 'e5228ac1aff65a038b7676c1623b0867d376d3576115ea924895ffef4576c7316fdb8c0eefb033eb864512844f41f44b4e80463fc066a1b638c6f240ff9079ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PocGA::Application.config.session_store :cookie_store, key: '_pocGA_session'
